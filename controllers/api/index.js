8f34f442057016668c943c0786fc7616dc375309
Orion888888 <151494847+Orion888888@users.noreply.github.com>
2024-05-04 15:17:56 -0500

    made changes to the seed.js file.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
 const postRoutes = require('./postRoutes');
-const userRoutes = require('./userRoutes');
+const userRoutes = require('./newuserRoutes');
 
 router.use('/user', userRoutes);
-router.use('/blog', blogRoutes);
+router.use('/comment', commentRoutes);
 router.use('/post', postRoutes);
 
 module.exports = router;
