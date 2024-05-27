82ca947e22cff8617ec3c18f628eb3480bf70ce8
Orion888888 <151494847+Orion888888@users.noreply.github.com>
2024-05-27 16:06:32 -0500

    added new code to the editPost to ensure commenting is working.

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,28 +1,35 @@
 const editPostHandler = async (event) => {
-    event.preventDefault();
-  
-    const title = document.querySelector('#post-title').value.trim();
-    const content = document.querySelector('#post-content').value.trim();
-    const id = window.location.toString().split('/')[window.location.toString().split('/').length-1];
-    const idVal = id.replace('?', '');
+  event.preventDefault();
 
-    console.log(idVal);
-      const response = await fetch(`/api/post/edit/${idVal}`, {
+  const title = document.querySelector('#post-title').value.trim();
+  const content = document.querySelector('#post-content').value.trim();
+  const id = window.location.pathname.split('/').pop();
+
+  if (title && content) {
+    try {
+      const response = await fetch(`/api/post/edit/${id}`, {
         method: 'PUT',
-        body: JSON.stringify({ id, title, content }),
+        body: JSON.stringify({ title, content }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
-  
+
       if (response.ok) {
         document.location.replace('/dashboard');
       } else {
-        alert('Failed to update a post');
+        const errorData = await response.json();
+        alert(`Failed to update the post: ${errorData.message}`);
       }
-  };
-  
-  
-  document
-    .querySelector('.edit-post-form')
-    .addEventListener('submit', editPostHandler);
+    } catch (error) {
+      console.error('Error:', error);
+      alert('An error occurred while updating the post.');
+    }
+  } else {
+    alert('Please fill out both the title and content fields.');
+  }
+};
+
+document
+  .querySelector('.edit-post-form')
+  .addEventListener('submit', editPostHandler);
