474f5908568ce6fe867fa06b45d04e88eb5552f4
Orion888888 <151494847+Orion888888@users.noreply.github.com>
2024-06-02 12:05:35 -0500

    fix sequelize connection.

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,7 @@ let sequelize;
 if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
-
   sequelize = new Sequelize(
-
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
