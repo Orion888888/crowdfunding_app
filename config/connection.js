252a8b40ab424faa67cf0f3aebaf3c462d1c5ade
Orion888888 <151494847+Orion888888@users.noreply.github.com>
2024-06-02 11:45:09 -0500

    fix sequelize connection.

--- config/connection.js
+++ config/connection.js
@@ -6,20 +6,17 @@ let sequelize;
 if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL);
 } else {
-  const dbName = process.env.DB_NAME;
-  const dbUser = process.env.DB_USER;
-  const dbPassword = process.env.DB_PASSWORD;
 
-  if (!dbName || !dbUser || !dbPassword) {
-    console.error('Environment Variables are not set correctly');
-    process.exit(1);
-  }
+  sequelize = new Sequelize(
 
-  sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432, // Default PostgreSQL port
-  });
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres'
+    }
+  );
 }
 
 module.exports = sequelize;
